433bb94bb392477da2740fcbda3e95c7316f9e72
Utkarsh Dubey <97157730+utkarshdubey2008@users.noreply.github.com>
2026-03-17 21:17:50 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,7 @@
+// Copyright (c) 2026 AlphaBotz & Adarsh
+// GitHub: https://github.com/TeamAlphabotz
+// GitHub: https://github.com/utkarshdubey2008
+// Telegram: @alter69x, @akanesakuramori
 import path from 'path';
 import fs from 'fs/promises';
 import { pathToFileURL } from 'url';
